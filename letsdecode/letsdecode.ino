15431345eb28fa91480aa0245a3e0c8bd4b92b29
Aniket Sharma <aniket965.as@gmail.com>
2020-11-24 11:22:13 +0530

    Update letsdecode.ino

--- letsdecode/letsdecode.ino
+++ letsdecode/letsdecode.ino
@@ -6,56 +6,29 @@
 int RECV_PIN = 11;
 unsigned int rawCodes[70];
 int codeLen;
-
-IRrecv irrecv(RECV_PIN);
-
-decode_results results;
+IRrecv IrReceiver(11);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Enabling IRin");
-  irrecv.enableIRIn(); // Start the receiver
   Serial.println("Enabled IRin");
+    IrReceiver.enableIRIn();  // Start the receiver
+    IrReceiver.blink13(true); // Enable feedback LED
+
+    Serial.print(F("Ready to receive IR signals at pin "));
+ 
 }
+
+
+
 
 void loop() {
-  if (irrecv.decode(&results))
-  { 
-      extractRaw(&results);
-      Serial.println("enedd");
-          // Displays the raw codes
-    for (int i = 0; i < codeLen; i ++)
-    {
-      Serial.print(rawCodes[i]);
-      Serial.print(", ");
+      if (IrReceiver.decode()) {  // Grab an IR code
+    
+        Serial.println();                               // blank line between entries
+        IrReceiver.printIRResultAsCArray(&Serial);      // Output the results as source code array
+       
+        IrReceiver.resume();                            // Prepare for the next value
     }
-    Serial.println(); 
-
-    irrecv.resume(); // Receive the next value
-      irrecv.resume(); // Receive the next value
-  }
-  delay(100);
 }
-// Extracts the raw data
-void extractRaw (decode_results *results) {
-  codeLen = results->rawlen - 1;
-  Serial.println(codeLen);
-  for (int i = 1; i <= codeLen; i++) {
-  
-    if (i % 2) {
-      // Mark
-      rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK - MARK_EXCESS;
-
-      
- 
-   }
-    else{
-      // Space
-      rawCodes[i - 1] = results->rawbuf[i]*USECPERTICK + MARK_EXCESS; 
-      }
-     
-      }
-}
-
-
